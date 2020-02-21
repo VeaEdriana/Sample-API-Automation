c1ed1ca7a93f3321f13fc325490b1d604f9b289a
Vea Edriana <vea.hufana@tooltwist.com>
2020-02-21 11:22:03 +0800

    Test Case Updates

---
+++ Libs/TempTestCase1582254966557.groovy
@@ -0,0 +1,24 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+import com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('/var/folders/13/q1xldntj6r559rm3tkz3qlvm0000gp/T/Katalon/Test Cases/API Test Case/20200221_111606/execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/API Test Case', new TestCaseBinding('Test Cases/API Test Case',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
